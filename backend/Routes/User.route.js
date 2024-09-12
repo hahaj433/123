9d805b50c752e53df0f1e394887d4245ffc374d1
an <tuanan298@icloud.com>
2024-09-12 19:40:42 +0700

    from an 123

--- backend/Routes/User.route.js
+++ backend/Routes/User.route.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const authController =require('../controller/authController')
 
 router.post("/register",authController.registerUser);
-
+// hello world
 module.exports = router
 bruh
